b5bbad51dc4398dd519d2fc475c6e595ce1c6395
Christoph Dalitz <christoph.dalitz@hs-niederrhein.de>
2025-02-21 14:29:15 +0100

    new release for CRAN prepared

---
+++ makecrandist.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#
+# Script for creating CRAN package
+# necessary due to different handling of LICENSE file on CRAN
+#
+
+BUILDDIR=build
+
+mkdir $BUILDDIR
+rsync -av ./ $BUILDDIR/ --exclude $BUILDDIR
+mv $BUILDDIR/lic_cran $BUILDDIR/LICENSE
+R CMD build $BUILDDIR
+rm -rf $BUILDDIR
